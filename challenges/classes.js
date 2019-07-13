cfa62d00057a771b99f505f419a35bfbd0bb21c7
Jacob Williams <JaWilliamsDev@gmail.com>
2019-07-12 20:52:25 -0400

    Finshed Stretch goal

--- challenges/classes.js
+++ challenges/classes.js
@@ -19,10 +19,30 @@ class RCuboidMaker{
 
 };
 
+class CubeMaker extends RCuboidMaker {
+    constructor(attb) {
+        super(attb)
+    }
+
+    volume() {
+        return (this.length ** 3)
+    }
+
+    surfaceArea() {
+        return (6 * this.length ** 2)
+    }
+};
+
 let rcuboid = new RCuboidMaker({
     length: 4,
     width: 5,
     height: 5,
+});
+
+let cube = new CubeMaker({
+    length: 3,
+    width: 3,
+    height: 3,
 });
 
     
@@ -30,3 +50,6 @@ console.log(rcuboid.volume()); // 100
 console.log(rcuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+console.log(cube.volume())
+console.log(cube.surfaceArea())
